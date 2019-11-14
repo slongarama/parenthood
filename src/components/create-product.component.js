1a50acd5725d758ecc0e8363625383490ed78287
Sabrina Long <sabrina.long3@gmail.com>
2019-11-13 23:12:44 -0500

    Backend <--> frontend connection done. Added ability to add products directly through interface

--- src/components/create-product.component.js
+++ src/components/create-product.component.js
@@ -1,11 +1,195 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateProduct extends Component {
-  render() {
-    return (
-      <div>
-        <p>You are on the Create Product component!</p>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeBrand = this.onChangeBrand.bind(this);
+        this.onChangeNewPrice = this.onChangeNewPrice.bind(this);
+        this.onChangeUsedPrice = this.onChangeUsedPrice.bind(this);
+        this.onChangeTrialPrice = this.onChangeTrialPrice.bind(this);
+        this.onChangeInventory = this.onChangeInventory.bind(this);
+        this.onChangeDate_Added = this.onChangeDate_Added.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: '',
+            description: '',
+            brand: '',
+            new_price: 0,
+            used_price: 0,
+            trial_price: 0,
+            inventory: 0,
+            date_added: new Date(),
+        }
+    }
+
+    componentDidMount() {
+        // this.setState({
+        //     name: 'Product name'
+        // });
+        axios.get('http://localhost:5000/users/')
+          .then(response => {
+            if (response.data.length > 0) {
+              this.setState({
+                // users: response.data.map(user => user.username),
+                name: response.data[0].username
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+    }
+
+    onChangeName(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+    onChangeBrand(e) {
+        this.setState({
+            brand: e.target.value
+        });
+    }
+    onChangeNewPrice(e) {
+        this.setState({
+            new_price: e.target.value
+        });
+    }
+    onChangeUsedPrice(e) {
+        this.setState({
+            used_price: e.target.value
+        });
+    }
+    onChangeTrialPrice(e) {
+        this.setState({
+            trila_price: e.target.value
+        });
+    }
+    onChangeInventory(e) {
+        this.setState({
+            inventory: e.target.value
+        });
+    }
+    onChangeDate_Added(date) {
+        this.setState({
+            date_added: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const product = {
+            name: this.state.name,
+            description: this.state.description,
+            brand: this.state.brand,
+            new_price: this.state.new_price,
+            used_price: this.state.used_price,
+            trial_price: this.state.trial_price,
+            inventory: this.state.inventory,
+            date_added: this.state.date_added,
+        };
+        console.log(product);
+
+        axios.post('http://localhost:5000/products/add', product)
+            .then(res => console.log(res.data));
+        window.location = '/';
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3>Add New Product</h3>
+                <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                    <label>Name: </label>
+                    <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.name}
+                    onChange={this.onChangeName}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Description: </label>
+                    <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.description}
+                    onChange={this.onChangeDescription}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Brand</label>
+                    <input
+                    type="text"
+                    className="form-control"
+                    value={this.state.brand}
+                    onChange={this.onChangeBrand}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>New Price</label>
+                    <input
+                    type="number"
+                    className="form-control"
+                    value={this.state.new_price}
+                    onChange={this.onChangeNewPrice}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Used Price</label>
+                    <input
+                    type="number"
+                    className="form-control"
+                    value={this.state.used_price}
+                    onChange={this.onChangeUsedPrice}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Trial Price</label>
+                    <input
+                    type="number"
+                    className="form-control"
+                    value={this.state.trial_price}
+                    onChange={this.onChangeTrialPrice}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Inventory</label>
+                    <input
+                    type="number"
+                    className="form-control"
+                    value={this.state.inventory}
+                    onChange={this.onChangeInventory}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Date: </label>
+                    <div>
+                        <DatePicker
+                        selected={this.state.date_added}
+                        onChange={this.onChangeDate_Added}
+                        />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <input type="submit" value="Add Product" className="btn btn-primary" />
+                </div>
+            </form>
+            </div>
+        )
+    }
 }
--- src/components/products-list.component.js
+++ src/components/products-list.component.js
@@ -1,12 +1,76 @@
-
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Product = props => (
+  <tr>
+    <td>{props.product.name}</td>
+    <td>{props.product.description}</td>
+    <td>{props.product.brand}</td>
+    <td>{props.product.new_price}</td>
+    <td>{props.product.used_price}</td>
+    <td>{props.product.trial_price}</td>
+    <td>{props.product.inventory}</td>
+    <td>{props.product.date_added.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.product._id}>edit</Link> | <a href="#" onClick={() => { props.deleteProduct(props.product._id) }}>delete</a>
+    </td>
+  </tr>
+)
 
 export default class ProductsList extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteProduct = this.deleteProduct.bind(this);
+        this.state = {products: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/products/')
+        .then(response => {
+            this.setState({ products: response.data });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    deleteProduct(id) {
+        axios.delete('http://localhost:5000/products/'+id)
+        .then(res => console.log(res.data));
+        this.setState({
+            products: this.state.products.filter(el => el._id !== id)
+        })
+    }
+
+    productList() {
+        return this.state.products.map(currentproduct => {
+            return <Product product={currentproduct} deleteProduct={this.deleteProduct} key={currentproduct._id}/>;
+        })
+    }
   render() {
     return (
-      <div>
-        <p>You are on the Products List component!</p>
-      </div>
+        <div>
+            <h3>All Products</h3>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Brand</th>
+                      <th>New Price</th>
+                      <th>Used Price</th>
+                      <th>Trial Price</th>
+                      <th>Inventory</th>
+                      <th>Date Added</th>
+                      <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  { this.productList() }
+                </tbody>
+            </table>
+        </div>
     )
   }
 }
