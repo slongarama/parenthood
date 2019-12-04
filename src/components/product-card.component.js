4b37062bdfe69f63b8ff8d326b8cf121c930028c
Sanya Nijhawan <sanya301@gmail.com>
2019-12-04 13:14:23 -0500

    upadted product card page

--- src/components/product-card.component.js
+++ src/components/product-card.component.js
@@ -12,7 +12,7 @@ export default class ProductCard extends Component {
 
         axios.get('http://localhost:5000/products/' + id)
             .then(response => {
-                this.setState({ product: response.data });
+                this.setState({ loading: false, product: response.data });
             })
             .catch((error) => {
                 console.log(error);
@@ -21,12 +21,23 @@ export default class ProductCard extends Component {
 
     render() {
         return (
-            <div className="product">
-                <div className="product__information">
-                    <h2 className="Product-title">{this.state.product.name}</h2>
-                </div>
-                <div className="product__description">
-                    ${this.state.product.description}/month
+            <div>
+                <div className="container">
+                    <div className="row align-items-stretch">
+                        <div className="col-lg-4">
+                            <img src={this.state.product.photo} vspace="20" alt="" className="img-fluid" />
+                        </div>
+                        <div class="col-lg-8">
+                            <div className="h-100 bg-white box-29291">
+                                <div className="product__information">
+                                    <h2 className="Product-title">{this.state.product.name}</h2>
+                                </div>
+                                <div className="product__description">
+                                    <p>{this.state.product.description} </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
